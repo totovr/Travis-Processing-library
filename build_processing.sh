96f65d4256c0b8291bccd65064cba9ad8dba4196
totovr <a.vegaramirez76@gmail.com>
2018-02-17 22:02:55 +0900

    Update

--- build_processing.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-git clone https://github.com/processing/processing.git
-cd processing
-cd build
-ant run
--- download_p5.sh
+++
@@ -1,9 +0,0 @@
-zipfile="controlp5/archive/master.zip"
-
-
-if [ ! -e $zipfile ]
-then
-  wget https://github.com/sojamo/$zipfile
-fi
-
-tar zxvf $zipfile > /dev/null
